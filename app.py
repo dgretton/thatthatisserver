04d6413a44805fdc68bdb7ca4d4522a4828395b3
Dana Gretton <dana.gretton@gmail.com>
2019-11-05 04:22:45 -0500

    nothing, nothing...

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ das_module_path = os.path.abspath('./stardas_v0')
 if das_module_path not in sys.path:
     sys.path.append(das_module_path)
 
-from stardas import dastag
+#from stardas import dastag
 from flask import Flask, render_template, request, jsonify
 
 from that_is_not_calculus import gen_thatthatis
@@ -20,6 +20,7 @@ def that_is_svc():
 
 @app.route("/genmeadastag/")
 def host_dastag():
+    return str((os.path.abspath('./stardas_v0'), sys.path))
     return dastag()
 
 if __name__ == '__main__':
